db59da82e117141ba84d7e5fd7eba3f2e3c2e250
Nolan Hoenert <192299544+nolanh3904@users.noreply.github.com>
2026-05-28 01:13:54 -0500

    Add: longest-substring-without-duplicates - submission-0

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        int left = 0;
+        int max = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (map.containsKey(c) && map.get(c) >= left) {
+                left = map.get(c) + 1;
+            }
+            map.put(c, i);
+            max = Math.max(max, i - left + 1);
+        }
+        return max;
+    }
+}
